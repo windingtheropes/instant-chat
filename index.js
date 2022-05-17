acced8b71e7f25113ab8bfed6918892c75d852d7
windingtheropes <windingtheropes@gmail.com>
2022-05-17 14:08:46 -0400

    mor

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+var names = []
+
 var messages = []
 
-function printUserCount() {console.log(`There are ${activeUsers} user(s) online.`)}
+function printUserCount() { console.log(`There are ${activeUsers} user(s) online.`); io.emit('userCountUpdated', activeUsers) }
 
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -27,15 +29,24 @@ io.on('connection', (socket) => {
   messages.forEach(m => socket.emit('message', m))
 
   socket.on('message', (msg) => {
-    if(msg.content.toLowerCase() === '/clear')
-    { 
-      messages = []
-      io.emit('reload')
+    if (msg.content.startsWith('/')) {
+      const command = msg.content.substring(1).toLowerCase()
+      const args = command.split(' ')
+      args.shift()
+      switch(command)
+      {
+        case 'clear':
+          messages = []
+          io.emit('reload')
+      }
+      return
     }
+    if(names.find(i => i.name === msg.name)) return
+    names.push(msg.name)
 
     messages.push(msg)
     io.emit('message', msg)
-    })
+  })
 
   socket.on('disconnect', () => {
     console.log('A user disconnected');
